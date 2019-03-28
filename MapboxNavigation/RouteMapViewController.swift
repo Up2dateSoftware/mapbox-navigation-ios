78c41fd1e6251ed50f8f5f4cbcc842e4d872a3fb
Georgiana <georgiana.tanase@up2date-software.ro>
2019-03-28 18:20:16 +0200

    Keyboard animation deleted.

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -916,9 +916,9 @@ extension RouteMapViewController {
     @objc fileprivate func keyboardWillShow(notification: NSNotification) {
         guard navigationView.endOfRouteView != nil else { return }
         guard let userInfo = notification.userInfo else { return }
-        let curve = UIViewAnimationCurve(rawValue: userInfo[UIKeyboardAnimationCurveUserInfoKey] as! Int)
-        let options = (duration: userInfo[UIKeyboardAnimationDurationUserInfoKey] as! Double,
-                       curve: curve!)
+//        let curve = UIViewAnimationCurve(rawValue: userInfo[UIKeyboardAnimationCurveUserInfoKey] as! Int)
+//        let options = (duration: userInfo[UIKeyboardAnimationDurationUserInfoKey] as! Double,
+//                       curve: curve!)
         let keyboardHeight = (userInfo[UIKeyboardFrameEndUserInfoKey] as! CGRect).size.height
 
         if #available(iOS 11.0, *) {
@@ -927,8 +927,11 @@ extension RouteMapViewController {
             navigationView.endOfRouteShowConstraint?.constant = -1 * keyboardHeight
         }
         
-        let opts = UIViewAnimationOptions(curve: options.curve)
-        UIView.animate(withDuration: options.duration, delay: 0, options: opts, animations: view.layoutIfNeeded, completion: nil)
+//        let opts = UIViewAnimationOptions(curve: options.curve)
+//        UIView.animate(withDuration: options.duration, delay: 0, options: opts, animations: view.layoutIfNeeded, completion: nil)
+        
+        view.layoutIfNeeded
+        
     }
     
     @objc fileprivate func keyboardWillHide(notification: NSNotification) {
